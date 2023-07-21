540420403d074a36bbfd63c000c855e577f57796
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2023-07-21 18:59:22 -0300

    add cors

--- peditz/settings.py
+++ peditz/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +72,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    '*',
 ]
 
 ROOT_URLCONF = 'peditz.urls'
