0216c6db520ed4ecdd8623d2b38012bcb6986ff4
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2023-10-03 09:55:44 -0300

    add blank in products

--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -219,7 +219,7 @@ class ProductComplementCategory(TimeStampedModel, UUIDModel):
     ))
     max_value = models.IntegerField(default=0)
     min_value = models.IntegerField(default=0)
-    products = models.ManyToManyField(Product, related_name='complement_categories')
+    products = models.ManyToManyField(Product, related_name='complement_categories', blank=True)
     restaurant = models.ForeignKey(
         Restaurant, on_delete=models.CASCADE, related_name='complement_categories')
     def __str__(self):
